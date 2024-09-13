776fb7b61f267cf13df2ed83d947035e8ca8ff94
Grayson <78488596+GraysonDeiley@users.noreply.github.com>
2024-09-13 15:47:25 -0400

    Delete components directory

--- components/navbar/NavSearch.tsx
+++
@@ -1,9 +0,0 @@
-import { Input} from "../ui/input";
-
-function NavSearch() {
-  return (
-    <Input type='text' placeholder='find a wave.' className="max-w-xs dark:bg-muted"></Input>
-  )
-}
-
-export default NavSearch
