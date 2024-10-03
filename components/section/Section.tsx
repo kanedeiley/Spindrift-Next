135ffa8f68e5bfc19be82a032d9e4d7149c7838c
Grayson <78488596+GraysonDeiley@users.noreply.github.com>
2024-10-03 10:08:28 -0400

    Update Section.tsx

--- components/section/Section.tsx
+++ components/section/Section.tsx
@@ -1,23 +1,59 @@
-'use client'
+'use client';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
+
 function Section() {
-const pathname = usePathname()
+  const pathname = usePathname();
+
   return (
     <div className='flex gap-10 mb-10 justify-center w-full items-center'>
-        <Link href="/journal" className={`p-2 capitalize rounded ${!pathname.includes("timeline") && !pathname.includes("create")  ? 'bg-muted': ''}`}>
-            Journal
-        </Link>
-         | 
-         <Link href="/journal/timeline" className={`p-2 capitalize rounded ${pathname.includes("timeline")  ? 'bg-muted': ''}`}>
-            Timeline
-        </Link>
-        |
-        <Link href="/journal/create" className={`p-2 capitalize rounded ${pathname.includes("create")  ? 'bg-muted': ''}`}>
-           Create
-        </Link>
-        </div>
-  )
+      <Link 
+        href="/journal" 
+        className={`p-2 capitalize rounded ${pathname && !pathname.includes("timeline") && !pathname.includes("create") ? 'bg-muted' : ''}`}
+      >
+        Journal
+      </Link>
+      |
+      <Link 
+        href="/journal/timeline" 
+        className={`p-2 capitalize rounded ${pathname && pathname.includes("timeline") ? 'bg-muted' : ''}`}
+      >
+        Timeline
+      </Link>
+      |
+      <Link 
+        href="/journal/create" 
+        className={`p-2 capitalize rounded ${pathname && pathname.includes("create") ? 'bg-muted' : ''}`}
+      >
+        Create
+      </Link>
+    </div>
+  );
 }
 
-export default Section
+export default Section;
+
+
+// 'use client'
+// import Link from 'next/link';
+// import { usePathname } from 'next/navigation'
+// function Section() {
+// const pathname = usePathname()
+//   return (
+//     <div className='flex gap-10 mb-10 justify-center w-full items-center'>
+//         <Link href="/journal" className={`p-2 capitalize rounded ${!pathname.includes("timeline") && !pathname.includes("create")  ? 'bg-muted': ''}`}>
+//             Journal
+//         </Link>
+//          | 
+//          <Link href="/journal/timeline" className={`p-2 capitalize rounded ${pathname.includes("timeline")  ? 'bg-muted': ''}`}>
+//             Timeline
+//         </Link>
+//         |
+//         <Link href="/journal/create" className={`p-2 capitalize rounded ${pathname.includes("create")  ? 'bg-muted': ''}`}>
+//            Create
+//         </Link>
+//         </div>
+//   )
+// }
+
+// export default Section
