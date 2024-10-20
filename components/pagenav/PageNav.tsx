dc52d2a2d636f4eb3be47c79a3fd89b6bb7f9a3c
Grayson <78488596+GraysonDeiley@users.noreply.github.com>
2024-10-20 19:38:51 -0400

    Update PageNav.tsx

--- components/pagenav/PageNav.tsx
+++ components/pagenav/PageNav.tsx
@@ -3,16 +3,17 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { SectionLink } from '@/utils/extratypes';
 
-function PageNav({pages}: {pages: SectionLink[]}) {
+function PageNav({ pages }: { pages: SectionLink[] }) {
   const pathname = usePathname();
   return (
     <div className='flex gap-4'>
-    {pages.map((page,i)=>(
-      <Link href={page.link}>{page.pagename}</Link>
-    )
-    )}
+      {pages.map((page, i) => (
+        <Link key={i} href={page.link}>
+          {page.pagename}
+        </Link>
+      ))}
     </div>
-  )}
+  );
+}
 
-
-export default PageNav
+export default PageNav;
